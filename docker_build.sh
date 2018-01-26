19261b99247f091dc784d115a4f7786dc5331fdc
Devin Deuville <devin.deuville@fcvinteractive.com>
2018-01-26 21:14:05 +0000

    change dockerbuild to use generic php info instead of ctc corporate specific config

--- docker_build.sh
+++ docker_build.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-
-echo "DO NOT USE UNTIL UPDATED: I'VE NOT DONE THIS... exiting."
-exit;
-
-
-
 echo "This script is supposed to be run by Jenkins build job."
 
-if [ -z "$DRUPAL_GIT_COMMIT" ]; then
-  echo "env: \$DRUPAL_GIT_COMMIT is expected (the commit hash from ctc/corporate-d8 repo)"
-  GIT_COMMIT=`git rev-parse --short --verify HEAD`
-  exit 1
-else
-  echo "Getting DRUPAL_GIT_COMMIT from env: $DRUPAL_GIT_COMMIT"
-fi
-
-IMAGE=corporate-d8
-
-PROJECT=https://nexus.fcvhost.com/repository/raw-ctc/ci/corporate/$DRUPAL_GIT_COMMIT/project.tgz
+IMAGE=web-php71-drupal
 
 # you can add user / pass like:
 #  -u jenkins:******
@@ -45,38 +29,23 @@ fi
 # --network host
 # otherwise it won't build
 
-# https://forums.docker.com/t/is-it-possible-to-specify-network-for-docker-build-command/26319/6
-docker build --pull -t nexus.fcvhost.com:5001/ci/$IMAGE:latest \
-  --build-arg project_artifact=$PROJECT \
-  --build-arg NEXUS_USER=jenkins \
-  --build-arg NEXUS_PASS=$JENKINS_PW \
-  --network host \
-  .
+# build and push using the 'latest' tag
+docker build --pull -t nexus.fcvhost.com:5000/php/$IMAGE:latest .
 
 ret=$?
 if [ $ret -ne 0 ]; then
-  echo "Error: docker build exit code = $ret"
+  echo "Build exit code = $ret"
   exit $ret
 fi
 
-docker push nexus.fcvhost.com:5001/ci/$IMAGE:latest
+docker push nexus.fcvhost.com:5000/php/$IMAGE:latest
 
-ret=$?
-if [ $ret -ne 0 ]; then
-  echo "Error: docker push exit code = $ret"
-  exit $ret
-fi
-
-# tag and push using the upstream Drupal project GIT COMMIT tag
-if [ "$DRUPAL_GIT_COMMIT" != "" ]; then
+# tag and push using other tags given
+if [ "$1" != "" ]; then
+  for tag in "$@"
+  do
     # tagging & pushing
-    docker tag nexus.fcvhost.com:5001/ci/$IMAGE:latest nexus.fcvhost.com:5001/ci/$IMAGE:$DRUPAL_GIT_COMMIT
-    docker push nexus.fcvhost.com:5001/ci/$IMAGE:$DRUPAL_GIT_COMMIT
-
-    ret=$?
-    if [ $ret -ne 0 ]; then
-      echo "Error: docker push tag $DRUPAL_GIT_COMMIT exit code = $ret"
-      exit $ret
-    fi
-
+    docker tag nexus.fcvhost.com:5000/php/$IMAGE:latest nexus.fcvhost.com:5000/php/$IMAGE:$tag
+    docker push nexus.fcvhost.com:5000/php/$IMAGE:$tag
+  done
 fi
